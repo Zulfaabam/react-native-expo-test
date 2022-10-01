2f0cc639915ca697f9f72069501db2827e8a8f70
Zulfaabam <zulfafatahakbar@gmail.com>
2022-10-01 21:17:44 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from "./pages/HomeScreen";
+import ProfileScreen from "./pages/ProfileScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import "react-native-gesture-handler";
+
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomTab.Navigator>
+        <BottomTab.Screen name="Home" component={HomeScreen} />
+        <BottomTab.Screen name="Profile" component={ProfileScreen} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ common/Data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    nama: "NAMA 1",
+    nim: "2112011XXXXXXX",
+    kelompok: "xx"
+  },
+  {
+    id: 2,
+    nama: "NAMA 2",
+    nim: "2112011XXXXXXX",
+    kelompok: "xx"
+  },
+  {
+    id: 3,
+    nama: "NAMA 3",
+    nim: "2112011XXXXXXX",
+    kelompok: "xx"
+  },
+  {
+    id: 4,
+    nama: "NAMA 4",
+    nim: "2112011XXXXXXX",
+    kelompok: "xx"
+  }
+];
